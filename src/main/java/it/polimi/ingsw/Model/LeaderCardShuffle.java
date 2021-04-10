9e13272291e0d7424fff03662aec54a535077179
michelangelorosa <michelangelo.rosa@mail.polimi.it>
2021-04-10 17:16:11 +0200

    Added the shuffler for Leader Cards and relative tests

---
+++ src/main/java/it/polimi/ingsw/Model/LeaderCardShuffle.java
@@ -0,0 +1,45 @@
+package it.polimi.ingsw.Model;
+
+import java.util.ArrayList;
+
+/**
+ * Invoked at the start of the game this class shuffles the Leader Cards for the players to chose
+*/
+public class LeaderCardShuffle {
+    private static boolean active = false;
+    static ArrayList<LeaderCard> LeaderCards;
+    //Reads the cards once
+    private static ArrayList<LeaderCard> leaderCardsManager(){
+        if(!active){
+            active=true;
+            LeaderCards = JSONReader.ReadLeaderCards();
+        }
+        return LeaderCards;
+    }
+    //Removes the cards
+    private static ArrayList<LeaderCard> leaderCardsManager(int remove){
+        LeaderCards.remove(remove);
+        return LeaderCards;
+    }
+    //Returns 4 different Leader Cards
+public static LeaderCard[] getLeaderShuffled(){
+    ArrayList<LeaderCard> LeaderCards = leaderCardsManager();
+    try{
+    int size = LeaderCards.size();
+    if(size<4)
+        return null;
+    LeaderCard[] cardsPicked = new LeaderCard[4];
+    for(int i=0;i<4;i++){
+        int rand = (int) (Math.random() * size);
+        cardsPicked[i]=LeaderCards.get(rand);
+        LeaderCards=leaderCardsManager(rand);
+        size--;
+    }
+        return cardsPicked;
+    }catch (ArrayIndexOutOfBoundsException e){
+        System.err.println("Error: no more Leader Cards!");
+        e.printStackTrace();
+    }
+        return null;
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/LeaderCardShuffleTest.java
@@ -0,0 +1,45 @@
+package it.polimi.ingsw;
+import it.polimi.ingsw.Model.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * Testing the Card Shuffler
+ */
+public class LeaderCardShuffleTest {
+
+    @Test
+    public void LeaderCardShuffleTests(){
+        LeaderCard[] cardsPicked1;
+        LeaderCard[] cardsPicked2;
+        LeaderCard[] cardsPicked3;
+        LeaderCard[] cardsPicked4;
+        //Testing the number of cards, each has to be different
+        cardsPicked1 = LeaderCardShuffle.getLeaderShuffled();
+        cardsPicked2 = LeaderCardShuffle.getLeaderShuffled();
+        cardsPicked3 = LeaderCardShuffle.getLeaderShuffled();
+        cardsPicked4 = LeaderCardShuffle.getLeaderShuffled();
+        assertEquals(cardsPicked1.length+cardsPicked2.length+cardsPicked3.length+cardsPicked4.length,16);
+        //Testing each card picked has a different id
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                assertNotEquals(cardsPicked1[i].getCardId(), cardsPicked2[j].getCardId());
+                assertNotEquals(cardsPicked1[i].getCardId(), cardsPicked3[j].getCardId());
+                assertNotEquals(cardsPicked1[i].getCardId(), cardsPicked4[j].getCardId());
+            }
+        }
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                assertNotEquals(cardsPicked2[i].getCardId(), cardsPicked3[j].getCardId());
+                assertNotEquals(cardsPicked2[i].getCardId(), cardsPicked4[j].getCardId());
+            }
+        }
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                assertNotEquals(cardsPicked3[i].getCardId(), cardsPicked4[j].getCardId());
+            }
+        }
+        //Testing that too many calls result in Error
+        assertNull(LeaderCardShuffle.getLeaderShuffled());
+    }
+
+}
