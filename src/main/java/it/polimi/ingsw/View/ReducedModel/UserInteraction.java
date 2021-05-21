775ce7ae47bb774de80a4904a325eb40254d53d8
redrick99 <10628467@polimi.it>
2021-05-22 01:15:15 +0200

    Added Javadoc.

--- src/main/java/it/polimi/ingsw/View/ReducedModel/UserInteraction.java
+++ src/main/java/it/polimi/ingsw/View/ReducedModel/UserInteraction.java
@@ -62,30 +62,45 @@ public abstract class UserInteraction extends Observable<Action> {
         return ui.init();
     }
 
+
+    /**
+     * Method used to ask the player if he wants to either Start a new game or connect to an
+     * existing one.
+     * @param clientConnection Used to send the result (boolean) of the User Interface interaction to the server.
+     * @throws Exception if there are problems when sending the message over the socket.
+     */
     public void startOrJoin(ClientConnection clientConnection) throws Exception{
         clientConnection.send(ui.startOrJoin());
     }
 
+    /**
+     * Method used to ask the player the number of players he wants the game to be composed of.
+     * @param clientConnection Used to send the result (int) of the User Interface interaction to the server.
+     * @throws Exception if there are problems when sending the message over the socket.
+     */
     public void numberOfPlayers(ClientConnection clientConnection) throws Exception {
         clientConnection.send(ui.numberOfPlayers());
     }
 
     /**
      * Method used to get the username from the player.
-     * @param clientConnection ClientConnection used to send the username.
+     * @param clientConnection Used to send the result (String) of the User Interface interaction to the server.
      * @throws Exception if there have been problems while serializing on the socket.
      */
     public void initialChooseName(ClientConnection clientConnection) throws Exception{
         clientConnection.send(ui.initialInsertName());
     }
 
+    /**
+     * Method used to display a message while the player waits for all the people to join.
+     */
     public void waitingForPlayers() {
         ui.waitingForPlayers();
     }
 
     /**
      * Method used to put the player inside a lobby where he has to select if he's ready.
-     * @param clientConnection ClientConnection used to send the boolean.
+     * @param clientConnection Used to send the result (boolean) of the User Interface interaction to the server.
      * @throws Exception if there have been problems while serializing on the socket.
      */
     public void initialLobby(ClientConnection clientConnection) throws Exception{
@@ -94,7 +109,7 @@ public abstract class UserInteraction extends Observable<Action> {
 
     /**
      * Method used to make the player choose his two Leader Cards.
-     * @param clientConnection ClientConnection used to send the chosen Leader Cards.
+     * @param clientConnection Used to send the result (ArrayList<LeaderCard>) of the User Interface interaction to the server.
      * @param leaderCards Four Leader Cards sent by the server to choose from.
      * @throws Exception if there have been problems while serializing on the socket.
      */
@@ -104,7 +119,7 @@ public abstract class UserInteraction extends Observable<Action> {
 
     /**
      * Method used to make the player choose his first Resources.
-     * @param clientConnection ClientConnection used to send the chosen Resources.
+     * @param clientConnection Used to send the result (ArrayList<Resources>) of the User Interface interaction to the server.
      * @param resources Number of resources the player has to choose.
      * @throws Exception if there have been problems while serializing on the socket.
      */
--- src/main/java/it/polimi/ingsw/View/UserInterface.java
+++ src/main/java/it/polimi/ingsw/View/UserInterface.java
@@ -9,23 +9,77 @@ import it.polimi.ingsw.View.ReducedModel.LeaderCard;
 
 import java.util.ArrayList;
 
+/**
+ * UserInterface Interface defines methods Overridden by both Cli and Gui classes to be used to get
+ * the user input, ready to be sent to the server via the UserInteraction Abstract Class.
+ */
 public interface UserInterface {
+    /**
+     * Used to get the Server Address and Server Port as input from the player.
+     * @return An ArrayList of object.
+     *  - First element in the ArrayList -> String containing server address.
+     *  - Second element in the ArrayList -> Integer containing server port.
+     */
     ArrayList<Object> init();
 
+    /**
+     * Used to get a boolean as input from the player to decide if he wants to start a new game or join
+     * an existing one.
+     * @return true  -> player wants to start a new game.
+     *         false -> player wants to join an existing game.
+     */
     boolean startOrJoin();
 
+    /**
+     * Used to get the number of players as input from the player who decided to start a new game.
+     * @return the number of players decided.
+     */
     int numberOfPlayers();
 
+    /**
+     * Used to get the UserName of the player as input.
+     * @return A String containing the UserName
+     */
     String initialInsertName();
 
+    /**
+     * Used to display a message while the player waits for all the other players to join.
+     */
     void waitingForPlayers();
 
+    /**
+     * Used to get a boolean as input from the player indicating whether he's ready to play or not.
+     * @return true if the player is ready.
+     */
     boolean initialLobby();
 
+    /**
+     * Used to get the initial LeaderCards as input from the player.
+     * @param leaderCards The 4 leader cards sent by the server to choose from.
+     * @return An Action Class message containing the 4 leader cards and methods to assign the 4 leader cards
+     * to the player inside the Model's Game.
+     * @throws IllegalArgumentException if the number of leader cards in the input ArrayList is != 4
+     */
     Action initialChooseLeaderCards(ArrayList<LeaderCard> leaderCards) throws IllegalArgumentException;
 
+    /**
+     * Used to get the initial Resources as input from the player.
+     * @param resources The number of resources the player has to choose.
+     * @return An Action Class message containing all resources chosen and the places chosen by the player
+     * to put said resources.
+     */
     Action initialChooseResources(int resources);
 
+    /**
+     * Used at the start of the actual Game turn to make the player choose which action he wants to do when
+     * playing the game. All methods below are mapped 1 to 1 with each Action Class message and are used to get
+     * a specific Action SUBCLASS which will be passed to this method.
+     * @param game Reduced Model game used to print/show the Game played.
+     * @return a specific Action Class message Subclass casted to its SuperClass and ready to be notified and sent
+     * to the Server.
+     * @throws IllegalStateException If the player does not have any possible action inside his
+     * ArrayList<ActionType> possibleActions.
+     */
     Action actionPicker(Game game) throws IllegalStateException;
 
     Action activateLeaderCard(Game game);
