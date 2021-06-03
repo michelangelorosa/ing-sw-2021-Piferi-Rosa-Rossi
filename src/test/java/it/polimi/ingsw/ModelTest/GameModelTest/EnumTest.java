52ae28506cd4c9cecdcd6499e79e545ecd5dddbb
redrick99 <10628467@polimi.it>
2021-06-03 15:43:01 +0200

    Updated tests for Enum Classes.

--- src/test/java/it/polimi/ingsw/ModelTest/GameModelTest/EnumTest.java
+++ src/test/java/it/polimi/ingsw/ModelTest/GameModelTest/EnumTest.java
@@ -1,4 +1,22 @@
 package it.polimi.ingsw.ModelTest.GameModelTest;
 
+import it.polimi.ingsw.Model.Enums.GameStatus;
+import it.polimi.ingsw.Model.Enums.ResourceType;
+import it.polimi.ingsw.View.Utility.ANSIColors;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class EnumTest {
+
+    @Test
+    public void GameStatusTest() {
+        GameStatus status = GameStatus.END;
+        assertEquals(GameStatus.END, status);
+    }
+
+    @Test
+    public void ResourceTypeTest() {
+        ResourceType type = ResourceType.COINS;
+        assertEquals(ANSIColors.FRONT_YELLOW + ANSIColors.COIN + ANSIColors.RESET, type.toCliSymbol());
+    }
 }
